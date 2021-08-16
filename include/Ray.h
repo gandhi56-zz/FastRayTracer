0035b9179500ebdf9eeb38e4e1f81cde89696c1c
gandhi56 <gandhi21299@gmail.com>
2021-08-16 01:36:03 -0600

    adding Vec3, Color and Ray classes

---
+++ include/Ray.h
@@ -0,0 +1,26 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "Vec3.h"
+
+template<typename T>
+class Ray {
+public:
+  Ray() {}
+  Ray(const Point3& origin, const Vec3<T>& direction)
+      : orig(origin), dir(direction)
+  {}
+
+  Point3 origin() const  { return orig; }
+  Vec3<T> direction() const { return dir; }
+
+  Point3 at(T t) const {
+      return orig + t*dir;
+  }
+
+public:
+  Point3 orig;
+  Vec3<T> dir;
+};
+
+#endif
